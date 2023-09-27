bd62b62ccf4edecdaf3eef9dd736411ae58453f9
Bunyod0301 <bunyodyusupov0@gmail.com>
2023-09-27 16:10:15 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Hello World Bunyod</h1>
+      <span>Hello</span>
     </>
   )
 }
