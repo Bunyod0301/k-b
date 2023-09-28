07e122d76d85c8ccd96b7177f7283eba6c8fe941
Bunyod0301 <bunyodyusupov0@gmail.com>
2023-09-28 15:20:38 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,8 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 
 import '../../public/assets/css/vendor/vendor.min.css';
 import '../../public/assets/css/plugins/plugins.min.css';
 import '../../public/assets/css/style.min.css';
-import Header from '@/components/header/Header';
-import Footer from '@/components/footer/footer';
-
-const inter = Inter({ subsets: ['latin'] })
 
 
 export const metadata: Metadata = {
@@ -22,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         {children}
       </body>
     </html>
